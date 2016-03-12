85fc3216688011765fdbb8299707c088e8e4cdb2
abelorihuelamendoza <abelorihuelamendoza@hotmail.com>
2016-03-11 19:48:24 -0600

    Add CORS

--- app/controllers/api/kindles_controller.rb
+++ app/controllers/api/kindles_controller.rb
@@ -1,10 +1,40 @@
 class Api::KindlesController < ApplicationController
 
+  before_filter :cors_preflight_check
+  after_filter :cors_set_access_control_headers
   respond_to :json
   PER_PAGE_RECORDS = 9
-
+  
   def index
     kindles_paginated = Kindle.order('id').page(params[:page]).per(PER_PAGE_RECORDS)
+    json_response = {
+      models: kindles_paginated,
+      current_page: params[:page].to_i,
+      perPage: PER_PAGE_RECORDS,
+      total_pages: kindles_paginated.num_pages
+    }
+    respond_with json_response, callback: params[:callback]
+  end
+
+  protected
+
+
+  def cors_set_access_control_headers
+    headers['Access-Control-Allow-Origin'] = '*'
+    headers['Access-Control-Allow-Methods'] = 'POST, GET, PUT, DELETE, OPTIONS'
+    headers['Access-Control-Allow-Headers'] = 'Origin, Content-Type, Accept, Authorization, Token'
+    headers['Access-Control-Max-Age'] = "1728000"
+  end
+
+  def cors_preflight_check
+    if request.method == 'OPTIONS'
+      headers['Access-Control-Allow-Origin'] = '*'
+      headers['Access-Control-Allow-Methods'] = 'POST, GET, PUT, DELETE, OPTIONS'
+      headers['Access-Control-Allow-Headers'] = 'X-Requested-With, X-Prototype-Version, Token'
+      headers['Access-Control-Max-Age'] = '1728000'
+
+      render :text => '', :content_type => 'text/plain'
+    end
   end
 
 end
