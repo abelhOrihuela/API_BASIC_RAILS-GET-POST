65da7eb890484260b16a35605a522d27e77b48f5
abelorihuelamendoza <abelorihuelamendoza@hotmail.com>
2016-03-17 10:43:26 -0600

    Create Kindle OK

--- app/controllers/api/kindles_controller.rb
+++ app/controllers/api/kindles_controller.rb
@@ -4,6 +4,8 @@ class Api::KindlesController < ApplicationController
   respond_to :json
   PER_PAGE_RECORDS = 9
 
+  skip_before_filter :verify_authenticity_token
+
   def index
     kindles_paginated = Kindle.order('id').page(params[:page]).per(PER_PAGE_RECORDS)
     json_response = {
@@ -17,19 +19,33 @@ class Api::KindlesController < ApplicationController
     #respond_with kindles_paginated , callback: params[:callback]
   end
 
+  def new
+    @kindle = Kindle.new
+  end
+
   def create
-    a = Kindle.create(kindle_params)
-    respond_with kindle, location: nil
+
+    puts "***"+ kindle_params[:status]
+    kindle = Kindle.new kindle_params
+  
+
   end
 
   def show
-    respond_with kindle.find(params[:id])
+    respond_with Kindle.find(params[:id])
   end
 
   def update
-
+    respond_with Kindle.update(params[:id], kindle_params)
   end
 
+  def destroy
+    respond_with kindle.destroy(params[:id])
+  end
+
+  def kindle_params
+    params.require(:kindle).permit(:inventory_id, :status)
+  end
 
 
 end
